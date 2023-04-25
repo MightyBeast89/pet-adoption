b0225b0c66d8a67a8c9d997959eb7a78dd5ce71d
MightyBeast <variola@my.centennialcollege.ca>
2023-04-25 11:32:37 -0400

    created model

---
+++ server/model/pets.js
@@ -0,0 +1,89 @@
+import mongoose, { Schema } from "mongoose";
+import shelterUser from "./shelterUser";
+
+const petSchema = mongoose.Schema({
+  shelterId: {
+    type: Schema.Types.ObjectId,
+    ref: "shelterUser",
+  },
+  petName: {
+    type: String,
+    required: true,
+  },
+  petAge: {
+    type: Number,
+    required: true,
+  },
+  petCategory: {
+    type: String,
+    required: true,
+  },
+  gwithFirstPetOwner: {
+    type: String,
+    required: true,
+  },
+  canGetAlongWithOtherPets: {
+    type: String,
+    required: true,
+  },
+  canGetWithHumanAge: {
+    type: String,
+    required: true,
+  },
+  indoorOutdoor: {
+    type: String,
+    required: true,
+  },
+  petOusideHours: {
+    type: String,
+    required: true,
+  },
+  petWithOwnerAllTime: {
+    type: String,
+    required: true,
+  },
+  petStay: {
+    type: String,
+    required: true,
+  },
+  serviceAnimal: {
+    type: String,
+    required: true,
+  },
+  enthusiasticPet: {
+    type: String,
+    required: true,
+  },
+  playfulPet: {
+    type: String,
+    required: true,
+  },
+  laidbackPet: {
+    type: String,
+    required: true,
+  },
+  trainablePet: {
+    type: String,
+    required: true,
+  },
+  specialNeeds: {
+    type: String,
+    required: true,
+  },
+  petPhoto: {
+    type: String,
+    required: true,
+  },
+  medHistory: {
+    type: String,
+    required: true,
+  },
+  behaveIssue: {
+    type: String,
+    required: true,
+  },
+  vaccRecord: {
+    type: String,
+    required: true,
+  },
+});
